32984b1e131d1405a0fbf179cc943a06ba62c7c1
Jack Diederich <jack@hivefire.com>
2013-05-08 11:50:29 -0400

    syntax error fixed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="boilerpot",
-      version="0.9"
+      version="0.9",
       description="HTML content extraction",
       author="Jack Diederich",
       author_email="jackdied@gmail.com",
